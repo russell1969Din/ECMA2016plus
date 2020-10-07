0c825b9df726cb3c2aa3b70ce45c5dc6d80f16c7
russell1969Din <rasto@fermata.sk>
2020-10-07 18:22:16 +0200

    Úprava parametrov systému

--- setsParam.php
+++ setsParam.php
@@ -1,8 +1,8 @@
 <?php
-        $dbLocal        = "46.229.230.130";
-        $dbLogin        = "uk028700";
-        $dbName         = "uk028700db";
-        $dbPass         = "_mD_g!U6g";
+        $dbLocal        = "";
+        $dbLogin        = "";
+        $dbName         = "";
+        $dbPass         = "";
         $dbPort         = "";
         $mailFrom       = "no-reply@propertynetwork.sk";
         $adminID        = 2;
